f6cedec28bcba2c1c204c9c15f48b20e192e1db3
sepfish <40220408+sepfish@users.noreply.github.com>
2019-02-21 20:00:47 -0800

    First day of modulo 5

---
+++ src/PrimeOrNot.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class PrimeOrNot {
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+		String prime = JOptionPane.showInputDialog(null, "Give me any number");
+
+		int num = Integer.parseInt(prime);
+
+		for (int i = 2; i < num; i++) {
+			if (num % i == 2) {
+JOptionPane.showMessageDialog(null, "Your number tis composite!!!");
+			System.exit(0);
+			}
+		}
+JOptionPane.showMessageDialog(null,"Your number tis Prime!!!");
+	}
+
+}
