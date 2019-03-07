ce166935fcf30a5d4436ce45adab818c7e23372c
League <league@WTS8.attlocal.net>
2019-03-06 20:00:11 -0800

    Did alot
    yeah

--- src/PrimeOrNot.java
+++ src/PrimeOrNot.java
@@ -7,7 +7,7 @@ public class PrimeOrNot {
 		String prime = JOptionPane.showInputDialog(null, "Give me any number");
 
 		int num = Integer.parseInt(prime);
-
+ 
 		for (int i = 2; i < num; i++) {
 			if (num % i == 2) {
 JOptionPane.showMessageDialog(null, "Your number tis composite!!!");
@@ -17,4 +17,4 @@ JOptionPane.showMessageDialog(null, "Your number tis composite!!!");
 JOptionPane.showMessageDialog(null,"Your number tis Prime!!!");
 	}
 
-}
+}       
