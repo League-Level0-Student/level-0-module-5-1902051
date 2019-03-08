ecefec696a6f2417a3f8b65f24d6bc1f22c7a443
sepfish <40220408+sepfish@users.noreply.github.com>
2019-03-07 20:00:14 -0800

    Started for loop aontlet. Finished circle ring and pi and goofy names!

--- src/algorithms/GoofyNames.java
+++ src/algorithms/GoofyNames.java
@@ -21,23 +21,29 @@ public class GoofyNames {
 		for (int i = 0; i < name.length(); i++) {
 			System.out.println(name.charAt(i));
 
-		
+			// 4. Create a String variable to store the next character of the name
+			// using .substring(start, end)
+			// 5. Use MODULO to set this variable
+			// HINT: replace 'start' and 'end' to get String with character at i
+			String a = name.substring(i, i + 1);
+			// 5. Use MODULO to set this variable to upper case for EVEN characters
+			// and lower case for ODD characters.
+			if (i % 2 == 1) {
+				a = a.toLowerCase();
+				goofyName = goofyName + a;
+			}
 
-		// 4. Create a String variable to store the next character of the name
-		// using .substring(start, end)
-		// 5. Use MODULO to set this variab
-		// HINT: replace 'start' and 'end' to get String with character at i
-String a = name.substring(i, i+1);
-		// 5. Use MODULO to set this variable to upper case for EVEN characters
-		// and lower case for ODD characters.
-if (i% 2 == 1) {
-	a = a.toLowerCase() ;
-	goofyName= goofyName + a;
-	}
-		// 6. ADD this String (containing 1 char) to the goofyName String
+			else {
+
+				a = a.toUpperCase();
+				goofyName = goofyName + a;
+			}
+			// 6. ADD this String (containing 1 char) to the goofyName String
 		}
-		
+
 		// 7. Use pop-up to show user their Goofy name
 
+		System.out.println(goofyName);
+		
 	}
 }
