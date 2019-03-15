67ef64e2891cd3e784fc4a8a65006f9eec52ea06
sepfish <40220408+sepfish@users.noreply.github.com>
2019-03-14 20:02:28 -0700

    for loop gontelt

--- src/FourLoopGontlet.java
+++ src/FourLoopGontlet.java
@@ -1,45 +1,35 @@
 
 public class FourLoopGontlet {
-public static void main(String[] args) {
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}	
-	
+	public static void main(String[] args) {
+
+		 for (int i = 1 ; i < 101; i++) {
+			System.out.println(i);
+		}		
+		 
+		 for (int i = 100 ; i > 0; i--) {
+				System.out.println(i);
+			}
+		
+		 for (int i = 2 ; i < 101; i+=2) {
+				System.out.println(i);
+			}
+		 for (int i = 1 ; i < 101; i+=2) {
+				System.out.println(i);
+			}
+		 for (int i = 1 ; i < 501; i++) {
+				System.out.println(i);
+				
+				 for (int f = 2 ; f < 501; i+=2) {
+						System.out.println("Evan");
+					
+				 
+				 
+				 
+				 }
+		 
+		 }	
+		
+	}
+
 }
