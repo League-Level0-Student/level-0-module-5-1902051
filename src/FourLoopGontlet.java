6f3c6a70178fbeada3585d00b15d3b605b5e43a8
1902051 <league@WTS-IM-6.attlocal.net>
2019-03-28 20:01:34 -0700

    almost done with the gauntlet

--- src/FourLoopGontlet.java
+++ src/FourLoopGontlet.java
@@ -1,35 +1,50 @@
+import javax.swing.JOptionPane;
 
 public class FourLoopGontlet {
 
 	public static void main(String[] args) {
 
-		 for (int i = 1 ; i < 101; i++) {
+		for (int i = 1; i < 101; i++) {
 			System.out.println(i);
-		}		
-		 
-		 for (int i = 100 ; i > 0; i--) {
+		}
+
+		for (int i = 100; i > 0; i--) {
+			System.out.println(i);
+		}
+
+		for (int i = 2; i < 101; i += 2) {
+			System.out.println(i);
+		}
+		for (int i = 1; i < 101; i += 2) {
+			System.out.println(i);
+		}
+
+		for (int i = 1; i < 501; i++) {
+
+			if (i % 2 == 0) {
+				System.out.println(i + " Is Even");
+			}
+			if (i % 2 == 1) {
+				System.out.println(i + "Is 0dd");
+			}
+		}
+		
+		
+			for (int i = 7; i < 777; i += 7) {
 				System.out.println(i);
 			}
 		
-		 for (int i = 2 ; i < 101; i+=2) {
+			for (int i = 0; i < 2; i++) {
 				System.out.println(i);
-			}
-		 for (int i = 1 ; i < 101; i+=2) {
-				System.out.println(i);
-			}
-		 for (int i = 1 ; i < 501; i++) {
-				System.out.println(i);
+
 				
-				 for (int f = 2 ; f < 501; i+=2) {
-						System.out.println("Evan");
-					
-				 
-				 
-				 
-				 }
-		 
-		 }	
-		
+				for (int j = 0; j < 9; j++) {
+					System.out.println(i);
+
+				}
+			}
+			
+			
 	}
 
 }
